848a01cc6f6e8daad0808cf228d1f047d26c29da
LC Wadle <lcwadle@gmail.com>
2017-09-16 17:21:46 -0500

    Removed app.run() params

--- project.py
+++ project.py
@@ -328,4 +328,4 @@ def deleteBeer(brewery_id, beer_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run()
